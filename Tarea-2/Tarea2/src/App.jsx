26cd111d2c8f29b439696280da677e75058ec673
ErickAlvarez2412 <erickjeanlucalvarez@gmail.com>
2024-07-29 19:05:47 -0500

    Tarea 2

---
+++ Tarea-2/Tarea2/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='card'>
+      <h1>John Cena</h1>
+      <img src="https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2023/05/john-cena-3033274.jpg?tf=1200x1200" alt="Foto personaje" />
+      <p>
+      John Felix Anthony Cena es un luchador profesional, actor y autor estadounidense que primero saltó a la fama en la organización World Wrestling Entertainment y luego tuvo éxito en películas y libros. Cena comenzó a levantar pesas cuando era preadolescente y luego decidió seguir una carrera en culturismo.
+      </p>
+    </div>
+  );
+}
+
+export default App;
